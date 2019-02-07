6024b351a31ee477f52c5e97ba6a094a37df2061
Jacek <jpalczewski@gmail.com>
2019-02-07 21:23:37 +0100

    created models & django templates

--- items/models.py
+++ items/models.py
@@ -1,8 +1,22 @@
 from django.db import models
 
+
+class Category(models.Model):
+    name = models.CharField(verbose_name="Category name", unique=True)
+
+
+class Collection(models.Model):
+    collection_name = models.CharField(verbose_name='Collection name', max_length=200)
+    description = models.TextField(verbose_name='Description', blank=True, null=True)
+    parent_collection = models.ForeignKey('Collection', on_delete=models.PROTECT)
+
+
 class Item(models.Model):
-    item_name = models.CharField(max_length=200)
+    item_name = models.CharField(verbose_name="Item name", max_length=200)
+    count = models.PositiveIntegerField(verbose_name="Count")
+    description = models.TextField(verbose_name="Description")
+    location = models.ForeignKey('Collection', on_delete=models.PROTECT)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT)
 
     def __str__(self):
         return self.item_name
-
