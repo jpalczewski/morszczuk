17db736d1aa427faee521e71691d9322433b83a2
Jacek <jpalczewski@gmail.com>
2019-02-07 23:02:20 +0100

    model fixes, admin inlines

--- items/models.py
+++ items/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(verbose_name="Category name", unique=True, max_length=50)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
@@ -14,13 +17,16 @@ class Collection(models.Model):
     description = models.TextField(verbose_name='Description', blank=True, null=True)
     parent_collection = models.ForeignKey('Collection', on_delete=models.PROTECT, null=True, blank=True)
 
+    def __str__(self):
+        return self.collection_name
+
 
 class Item(models.Model):
     item_name = models.CharField(verbose_name="Item name", max_length=200)
-    count = models.PositiveIntegerField(verbose_name="Count")
-    description = models.TextField(verbose_name="Description")
+    count = models.PositiveIntegerField(verbose_name="Count", default=1)
+    description = models.TextField(verbose_name="Description", blank=True, null=True)
     location = models.ForeignKey('Collection', on_delete=models.PROTECT)
-    category = models.ForeignKey('Category', on_delete=models.PROTECT)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, blank=True, null=True)
 
     def __str__(self):
         return self.item_name
